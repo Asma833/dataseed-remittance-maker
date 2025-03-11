9b55be4cb5817ab7ecc66d6f264cf5e7dbf5971f
Asma <asma@dataseedtech.com>
2025-03-11 11:24:23 +0530

    dashboard: Replaced icons with images

--- src/features/checker/pages/dashboard/page/DashboardPage.tsx
+++ src/features/checker/pages/dashboard/page/DashboardPage.tsx
@@ -5,7 +5,7 @@ import DashboardCard from "./dashboard-card/DashboardCards";
 const DashboardPage = () => {
     return <div className="py-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
     {dashboardData.map((item) => (
-      <DashboardCard key={item.id} icon={item.icon} count={item.count} title={item.title} />
+      <DashboardCard key={item.id} path={item.path} count={item.count} title={item.title} />
     ))}
   </div>
   };
--- src/features/checker/pages/dashboard/page/dashboard-card/DashboardCards.tsx
+++ src/features/checker/pages/dashboard/page/dashboard-card/DashboardCards.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 interface DashboardCardProps {
-  icon: React.ElementType;
   count: number;
   title: string;
+  path:string;
 }
 
-const DashboardCard: React.FC<DashboardCardProps> = ({ icon: Icon, count, title }) => {
+const DashboardCard: React.FC<DashboardCardProps> = ({ count, title,path }) => {
   return (
     <div className="bg-gray-200 ease-linear shadow-md  flex items-center px-4 py-4 w-full  h-[120px] ">
       {/* Left: Icon */}
-      <div className="w-[50px] h-[50px] flex items-center justify-center rounded-full bg-gray-100">
-        <Icon className="w-8 h-8 text-gray-700" />
+      <div className="w-[50px] h-[50px] flex items-center justify-center">
+        <img src={path}/>
       </div>
 
       {/* Right: Text Content */}
       <div className="ml-4">
-        <h2 className="text-2xl font-semibold">{count.toLocaleString()}</h2>
+        <h2 className="text-2xl font-semibold">{count}</h2>
         <p className="text-sm ">{title}</p>
       </div>
     </div>
