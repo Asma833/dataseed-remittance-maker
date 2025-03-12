90b0744dab8dfd4fb71c513bcca13518f7720471
Asma <asma@dataseedtech.com>
2025-03-12 22:02:07 +0530

    style(dashboard-card): improved visuals and layout

--- src/features/checker/pages/dashboard/page/DashboardPage.tsx
+++ src/features/checker/pages/dashboard/page/DashboardPage.tsx
@@ -3,11 +3,20 @@ import DashboardCard from "./dashboard-card/DashboardCards";
 
 
 const DashboardPage = () => {
-    return <div className="py-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    return(
+    <>
+    <div className="px-2">
+    <p className="font-bold">Overview</p>
+    <p className="mb-4 mt-0">Monitor your transaction statuses and verification processes</p>
+
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
     {dashboardData.map((item) => (
-      <DashboardCard key={item.id} path={item.path} count={item.count} title={item.title} />
+      <DashboardCard key={item.id} id={item.id} status={item.status} path={item.path} count={item.count} title={item.title} />
     ))}
   </div>
+  </div>
+  </>
+    )
   };
   
 export default DashboardPage;
--- src/features/checker/pages/dashboard/page/dashboard-card/card.ts
+++ src/features/checker/pages/dashboard/page/dashboard-card/card.ts
@@ -15,17 +15,18 @@ export interface DashboardItem {
   count: number;
   icon: React.ComponentType; // Store icon as a component reference
   path:string;
+  status:string;
 }
 
 export const dashboardData: DashboardItem[] = [
-  { id: 1, title: "Transaction Received", count: 3100, icon: FileCheck2 ,path:img},
-  { id: 2, title: "Transaction Approved", count: 900, icon: CheckCircle,path:image2 },
-  { id: 3, title: "Transaction Rejected", count: 3100, icon: XCircle,path:image3},
-  { id: 4, title: "Transaction Pending", count: 1100, icon: MoreHorizontal,path:image4 },
-  { id: 5, title: "VKYC Completed", count: 1800, icon: FileCheck2,path:image5 },
-  { id: 6, title: "VKYC Pending", count: 800, icon: FileClock,path:image6 },
-  { id: 7, title: "VKYC Rejected", count: 800, icon: FileX,path:image7 },
-  { id: 8, title: "Esign Completed", count: 500, icon: FileCheck2,path:image8 },
-  { id: 9, title: "Esign Pending", count: 50, icon: FileClock,path:image9 },
-  { id: 10, title: "Esign Rejected", count: 200, icon: FileX,path:image10 },
+  { id: 1, title: "Transaction Received", count: 3100,status:"Received", icon: FileCheck2 ,path:img},
+  { id: 2, title: "Transaction Approved", count: 900,status:"Approved", icon: CheckCircle,path:image2 },
+  { id: 3, title: "Transaction Rejected", count: 3100,status:"Rejected", icon: XCircle,path:image3},
+  { id: 4, title: "Transaction Pending", count: 1100,status:"Pending", icon: MoreHorizontal,path:image4 },
+  { id: 5, title: "VKYC Completed", count: 1800,status:"Completed", icon: FileCheck2,path:image5 },
+  { id: 6, title: "VKYC Pending", count: 800,status:"Pending", icon: FileClock,path:image6 },
+  { id: 7, title: "VKYC Rejected", count: 800,status:"Rejected", icon: FileX,path:image7 },
+  { id: 8, title: "Esign Completed", count: 500,status:"Completed", icon: FileCheck2,path:image8 },
+  { id: 9, title: "Esign Pending", count: 50,status:"Pending", icon: FileClock,path:image9 },
+  { id: 10, title: "Esign Rejected", count: 200,status:"Rejected", icon: FileX,path:image10 },
 ];
