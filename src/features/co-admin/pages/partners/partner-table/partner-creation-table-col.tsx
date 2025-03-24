b09eb2f37a8c5f30b956449413aaf5fa1d521607
Asma <asma@dataseedtech.com>
2025-03-24 23:19:09 +0530

    chore(partner): Updated navigation route

--- src/features/co-admin/pages/partners/partner-table/partner-creation-table-col.tsx
+++ src/features/co-admin/pages/partners/partner-table/partner-creation-table-col.tsx
@@ -73,8 +73,7 @@ export const getUserTableColumns = (
           <div className="flex flex-col items-center">
             <button
               className="p-2 rounded-md hover:bg-muted/20"
-              onClick={() =>{console.log(rowData,"rowData");
-                handleNavigate(`update-user/${rowData.hashed_key}`,rowData)}}
+              onClick={() =>handleNavigate(`update-partner/${rowData.hashed_key}`,rowData)}
             >
               <Edit className="w-5 h-5 text-muted-foreground" />
             </button>
