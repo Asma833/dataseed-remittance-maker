73afeea31726a61c697e06cfddea22fda16b148f
Asma <asma@dataseedtech.com>
2025-06-05 15:45:29 +0530

    column creation

--- src/features/maker/pages/view-status/VIewStatus.tsx
+++ src/features/maker/pages/view-status/VIewStatus.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+// import React from 'react';
 
-type Props = {};
+// type Props = {};
 
-const VIewStatus = (props: Props) => {
-  return <div>VIewStatus</div>;
-};
+// const VIewStatus = (props: Props) => {
+//   return <div className="dynamic-table-wrap">
+//         <DynamicTable
+//           columns={tableColumns}
+//           data={tableData}
+//           defaultSortColumn="niumId"
+//           defaultSortDirection="asc"
+//           loading={isLoading}
+//           refreshAction={{
+//             isRefreshButtonVisible: true,
+//             onRefresh: refreshData,
+//             isLoading: isLoading,
+//             hasError: hasError,
+//           }}
+//           paginationMode={'static'}
+//           onPageChange={
+//             isPaginationDynamic ? pagination.handlePageChange : async (_page: number, _pageSize: number) => []
+//           }
+//           filter={{
+//             filterOption: true,
+//             mode: 'static',
+//             dateFilterColumn: 'created_at',
+//             renderFilterOptions: {
+//               search: true,
+//               dateRange: true,
+//               applyAction: true,
+//               resetAction: true,
+//               selects: [
+//                 {
+//                   id: 'purpose_type_name.purpose_name',
+//                   label: 'Purpose Type',
+//                   placeholder: 'Select',
+//                   options: purposeTypeOptions,
+//                 },
+//                 {
+//                   id: 'transaction_type_name.name',
+//                   label: 'Transaction Type',
+//                   placeholder: 'Select',
+//                   options: transactionTypeOptions,
+//                 },
+//               ],
+//             },
+//           }}
+//         />;
+//         </div>
+// };
 
-export default VIewStatus;
+// export default VIewStatus;
---
+++ src/features/maker/pages/view-status/ViewStatusColumns.tsx
@@ -0,0 +1,70 @@
+export const GetViewStatusColumns = ({
+    
+})=>[
+    {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'Transaction No',
+    className: 'min-w-0 p-2'
+  },
+    {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'Created Date',
+    className: 'min-w-0 p-2'
+  },
+    {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'Expiry Date',
+    className: 'min-w-0 p-2'
+  },
+    {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'Applicant Name',
+    className: 'min-w-0 p-2'
+  },
+{
+    key: 'nium_order_id',
+    id: 'Applicant PAN No',
+    name: 'Applicant PAN No',
+    className: 'min-w-0 p-2'
+  },
+    {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'Transaction Type',
+    className: 'min-w-0 p-2'
+  },
+  {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'Purpose',
+    className: 'min-w-0 p-2'
+  },
+  {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'Transaction Status',
+    className: 'min-w-0 p-2'
+  },
+  {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'E Sign Status',
+    className: 'min-w-0 p-2'
+  },
+  {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'E Sign Link',
+    className: 'min-w-0 p-2'
+  },
+  {
+    key: 'nium_order_id',
+    id: 'nium_order_id',
+    name: 'View',
+    className: 'min-w-0 p-2'
+  }
+]
