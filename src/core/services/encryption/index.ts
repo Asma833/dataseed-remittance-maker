94f78c8eb5b015dd64bcfdafa5c6d8da89045001
omraj-dataseed <omraj@dataseedtech.com>
2025-06-18 12:11:52 +0530

    refactor: Remove EncryptedApiUtils export from encryption service index

--- src/core/services/encryption/index.ts
+++ src/core/services/encryption/index.ts
@@ -5,9 +5,6 @@ export type { EncryptionResult, DecryptionParams, ApiEncryptionPayload } from '.
 // Axios interceptors
 export { encryptRequestInterceptor, decryptResponseInterceptor, EncryptedApiClient } from './encryptionInterceptor';
 
-// Utility functions
-export { EncryptedApiUtils } from './encryptedApiUtils';
-
 // React hook
 export { useEncryption } from '@/hooks/useEncryption';
 
