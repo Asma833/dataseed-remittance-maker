aa83421e15097e6b1ae4de01a8372cb7352846f5
Asma <asma@dataseedtech.com>
2025-08-12 16:15:00 +0530

    feat: Add husband and wife options to paidByOptions in transaction form configuration

--- src/components/table/common-tables/n-user/user-creation-form/UserCreationFormPage.tsx
+++ src/components/table/common-tables/n-user/user-creation-form/UserCreationFormPage.tsx
@@ -90,7 +90,7 @@ const UserCreationFormPage = ({ formConfig, schema, role }: UserCreationFormPage
         <FormContentWrapper className="p-2 rounded-lg mr-auto bg-transparent">
           <h2 className="text-xl font-bold mb-4 title-case">{pageTitle}</h2>
           <Spacer>
-            <FormFieldRow className="mb-4">
+            <FormFieldRow>
               <FieldWrapper>
                 {getController({
                   ...formConfig.fields.email,
@@ -111,7 +111,7 @@ const UserCreationFormPage = ({ formConfig, schema, role }: UserCreationFormPage
                 </div>
               </FieldWrapper> */}
             </FormFieldRow>
-            <FormFieldRow className="mb-4 mt-1">
+            <FormFieldRow className="mt-1">
               {Object.entries(formConfig.fields)
                 .slice(2, 3)
                 .map(([name, field]) => (
@@ -125,7 +125,7 @@ const UserCreationFormPage = ({ formConfig, schema, role }: UserCreationFormPage
                   </FieldWrapper>
                 ))}
             </FormFieldRow>
-            <FormFieldRow className="mb-4 mt-2">
+            <FormFieldRow className="mt-2">
               {Object.entries(formConfig.fields)
                 .slice(3, 4)
                 .map(([name, field]) => (
