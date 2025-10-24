c6f18799d4737943b3131d885b0ddbc7570b3971
Asma <asma@dataseedtech.com>
2025-10-24 19:29:46 +0530

    refactor(routes): update admin default route to include user management

--- src/core/constant/manage-default-routes.ts
+++ src/core/constant/manage-default-routes.ts
@@ -2,7 +2,7 @@ import { UserRole } from '../../features/auth/types/auth.types';
 import { ROUTES, getNavPath } from './route-paths';
 
 export const DEFAULT_ROUTES: Record<UserRole, string> = {
-  admin: getNavPath('ADMIN', ROUTES.ADMIN.SUPER_CHECKER_TABLE),
+  admin: getNavPath('ADMIN', ROUTES.ADMIN.USER_MANAGEMENT.SUPER_CHECKER_TABLE),
   checker: getNavPath('CHECKER', ROUTES.CHECKER.VIEWALL),
   maker: getNavPath('MAKER', ROUTES.MAKER.CREATE_TRANSACTION),
 } as const;
---
+++ src/features/remittance/master/rate-master/api/card-edit.api.ts
@@ -0,0 +1,12 @@
+import { CardEditFormData } from "../tabs/card/form/card-edit-form.schema";
+
+// Function to update card data
+export const updateCardData = async (cardData: CardEditFormData & { id: string }): Promise<CardEditFormData> => {
+  // Simulate API call
+  return new Promise((resolve) => {
+    setTimeout(() => {
+     // console.log('Updating card data:', cardData);
+      resolve(cardData);
+    }, 1000);
+  });
+};
